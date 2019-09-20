ba22bab86d580f8c1cf5b09eed1ecc2264245f93
Cl4wsCyber <55567957+Cl4wsCyber@users.noreply.github.com>
2019-09-20 09:25:34 +0700

    New upload

---
+++ CLAWS.sh
@@ -0,0 +1,172 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+ 
+ figlet -f slant "ClawsCyber"|lolcat
+
+echo $i"|"$me" 1"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 2"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" L"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" W"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" S"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me" 8"$i" |"$cy" PHISING FACEBOOK         "$i"|"$i"    |"$cy" Y"$i" |"
+echo $1"|"$me" 9"$1" |"%cy" Admin Finder             "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me"10"$i" |"$cy" NMAP                     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INSTALL BAHAN DULU ya    "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"12"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" ~"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 9 ]
+then 
+clear
+figlet -f slant "S A B A R"
+sleep 1	
+git clone  https://github.com/the-c0d3r/admin-finder.git
+cd admin-finder
+python2 admin-finder.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "S A B A R"
+sleep 1 
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 lazymux.py
+fi	
+
+if [ $pil = 11 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+exit
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Mungkin Kamu Yang Nub :v"
+sleep 2
+echo $ku"WhatsApp :"$i" 089515624429"
+echo $ku"Facebook :"$i" Rizky maulana"
+echo $ku"Youtube  :"$i" Claws Channel"
+sleep 2
+echo $pur">> Thanks YOU AND VIRUS DETECTED <<"
+exit
+fi
+
